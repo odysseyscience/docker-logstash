f44371d45b4924a00a99dad9fa98b31d97e116b0
Sean Adkinson <sean.adkinson@gmail.com>
2014-12-21 17:35:16 -0800

    Fixing es ip

--- env.sh
+++ env.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #
-# Purpose of this script is to populate environmental variables into logstash configuration file 
+# Purpose of this script is to populate environmental variables into logstash configuration file
 #
 
-sed -i 's|127.0.0.1|'$ELASTICSEARCH_IP'|g' /etc/logstash.conf
-
+sed -i 's|127.0.0.1|'$ELASTICSEARCH_IP'|g' /etc/logstash.d/logstash.conf
